3bf23811cec8f29ec47e4015b18988caa39f48bd
FateConcealed <angusyuen@Squall.local>
2015-04-25 23:10:55 +1000

    C almost

--- Lab03/Dynamic Pattern/Dynamic_Pattern.asm
+++ Lab03/Dynamic Pattern/Dynamic_Pattern.asm
@@ -1,11 +1,13 @@
 .include "m2560def.inc"
 
+.def tempOutput = r1
 .def temp = r16
 .def output = r17
 .def count = r18
 .def nBit = r19
 .def debounce = r20
 .def nFlash = r22
+//.def isNextInput = r23
 
 .macro clear
 	ldi YL, low(@0)
@@ -51,6 +53,8 @@ RESET:
 	clr debounce
 	clr r0
 	clr nFlash
+	//clr isNextInput
+	clr tempOutput
 
 	ser temp					; set temp to 0xFF
 	out DDRC, temp				; set DDRC to 0xFF, 8 pins, so setting 8 bits to 1 sets 8 pins for output
@@ -72,6 +76,8 @@ RESET:
 	jmp main
 
 Timer0OVF:
+/*	cpi isNextInput, 1
+	breq handleLights*/
 	cpi nBit, 8						; if we have 8 bits inputted
 	breq handleLights				; we need to flash the lights
 	cpi debounce, 0					; if debounce is not 0
@@ -90,23 +96,39 @@ Timer0OVF:
 		lds r25, TempCounter+1
 		adiw r25:r24, 1
 		
+		//cpi isNextInput, 1
+		//breq lastDisplay
+
 		cpi r24, low(7812)			; 256*8/clock speed, where clockspeed is 16MHz then 1000000/128 = 7812, 1 second
 		ldi temp, high(7812)		
 		cpc r25, temp
 		brne NotSecond
 
+	/*	cpi isNextInput, 1
+		breq lastBlank*/
+
 		in temp, PORTC
 		cp temp, r0					; compare current state of PORTC with 0
 		brne displayBlank			; if it is not equal, then we are currently displaying output, so now we want to display blank LEDs
-		cp temp, r0							; if the current state is all blank
-		breq displayOutput					; then we want to display the actual user inputted leds
-
+									; if the current state is all blank
+									; then we want to display the actual user inputted leds
+		
 		displayOutput:
 			inc nFlash
-			cpi nFlash, 4
+			cpi nFlash, 3
 			breq getNextInput
 			out PORTC, output
 			rjmp prologue
+
+		/*lastDisplay:
+			clr isNextInput
+			out PORTC, tempOutput
+			rjmp prologue*
+
+		lastBlank:
+			clr nFlash
+			out PORTC, r0
+			rjmp prologue*/
 
 		displayBlank:
 			out PORTC, r0
@@ -117,6 +139,7 @@ Timer0OVF:
 			clr output
 			clr nBit
 			clr debounce
+			clr nFlash
 			rjmp prologue
 
 	NotSecond:
@@ -131,17 +154,6 @@ Timer0OVF:
 		pop temp
 		out SREG, temp
 		reti
-
-	prologue:
-		clear TempCounter
-
-		lds r24, SecondCounter
-		lds r25, SecondCounter+1
-		adiw r25:r24, 1
-
-		sts SecondCounter, r24
-		sts SecondCounter+1, r25
-		rjmp EndIF
 
 	handleDebounce:
 		in temp, SREG
@@ -173,6 +185,18 @@ Timer0OVF:
 
 		rjmp EndIF
 
+	prologue:
+		clear TempCounter
+
+		lds r24, SecondCounter
+		lds r25, SecondCounter+1
+		adiw r25:r24, 1
+
+		sts SecondCounter, r24
+		sts SecondCounter+1, r25
+		rjmp EndIF
+
+
 EXT_INT0:
 	cpi debounce, 0
 	breq isDebounced0
@@ -188,13 +212,16 @@ EXT_INT0:
 		rcall checkPos
 		or output, temp
 
-		out PORTC, output
+		;out PORTC, output
 
 		pop temp
 		out SREG, temp
 		pop temp
 		inc nBit
 		clr count
+
+		cpi nBit, 8
+		breq display
 		reti
 
 EXT_INT1:
@@ -212,14 +239,21 @@ EXT_INT1:
 		rcall checkPos
 		or output, temp
 
-		out PORTC, output
+		;out PORTC, output
 
 		pop temp
 		out SREG, temp
 		pop temp
 		inc nBit
 		clr count
+
+		cpi nBit, 8
+		breq display
 		reti
+
+display:
+	out PORTC, output
+	reti
 
 // if nBit is 0, then that means this is the first user input
 // and it immediately returns
