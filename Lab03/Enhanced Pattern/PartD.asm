5374f6d73b1db862ecbfbc9eab55778a07118f83
FateConcealed <angusyuen@Squall.local>
2015-04-27 00:33:28 +1000

    part D

--- Lab03/Enhanced Pattern/PartD.asm
+++ Lab03/Enhanced Pattern/PartD.asm
@@ -7,7 +7,6 @@
 .def debounce = r20
 .def nFlash = r21
 .def isDisplaying = r22
-.def isInputting = r23
 
 .macro clear
 	ldi YL, low(@0)
@@ -21,7 +20,7 @@
 SecondCounter:
 	.byte 2
 
-TempCounter:
+MilliCounter:
 	.byte 2
 
 Pattern:
@@ -66,7 +65,6 @@ RESET:
 	clr nFlash
 	clr currOutput
 	clr isDisplaying
-	clr isInputting
 
 	ser temp					; set temp to 0xFF
 	out DDRC, temp				; set DDRC to 0xFF, 8 pins, so setting 8 bits to 1 sets 8 pins for output
@@ -88,40 +86,22 @@ RESET:
 	jmp main
 
 Timer0OVF:
-	init:
-		in temp, SREG				; store current state of status registers in temp
-		push temp					; push conflict registers onto the stack
-		push YH
-		push YL
-		push r25
-		push r24
+	in temp, SREG				; store current state of status registers in temp
+	push temp					; push conflict registers onto the stack
+	push YH
+	push YL
+	push r25
+	push r24
 
-		lds r24, TempCounter
-		lds r25, TempCounter+1
-		adiw r25:r24, 1
+	lds r24, SecondCounter
+	lds r25, SecondCounter+1
+	adiw r25:r24, 1
 
-		cpi isDisplaying, 0
-		brne lightTimer
-		cpi isInputting, 0
-		breq lightTimer
-		rjmp debounceTimer
-
-		lightTimer:
-			cpi r24, low(7812)			; 256*8/clock speed, where clockspeed is 16MHz then 1000000/128 = 7812, 1 second
-			ldi temp, high(7812)		
-			cpc r25, temp
-			brne NotSecond				; if it is not a second yet skip to notSecond
-			clr debounce
-			rjmp startDisplay
-
-		debounceTimer:
-			cpi r24, low(1953)			; 256*8/clock speed, where clockspeed is 16MHz then 1000000/128 = 7812, 1 second
-			ldi temp, high(1953)		
-			cpc r25, temp
-			brne NotSecond				; if it is not a second yet skip to notSecond
-			clr debounce				; past this is one second
-			out PORTC, r0
-			rjmp finish;
+	timeSecond:
+		cpi r24, low(7812)			; 256*8/clock speed, where clockspeed is 16MHz then 1000000/128 = 7812, 1 second
+		ldi temp, high(7812)		
+		cpc r25, temp
+		brne NotSecond				; if it is not a second yet skip to notSecond
 
 	; on less than 8 bits, it will skip through all of these and go down
 	; to flashDisplay
@@ -145,14 +125,9 @@ Timer0OVF:
 	clearLights:
 		out PORTC, r0
 		clr isDisplaying
-		cpi nFlash, 3
-		brlo finish
-		
-	clearCurrOutput:
-		clr currOutput 
 
 	finish:
-		clear TempCounter   		; reset the temporary counter.
+		clear SecondCounter 		; reset the temporary counter.
 	    lds r24, SecondCounter		; loading value of second counter
 	    lds r25, SecondCounter+1
 	    adiw r25:r24, 1     		; increase second counter by 1
@@ -162,8 +137,25 @@ Timer0OVF:
 	    rjmp EndIF
 
 	NotSecond:
-		sts TempCounter, r24
-		sts TempCounter+1, r25
+		sts SecondCounter, r24
+		sts SecondCounter+1, r25
+
+	checkMilli:
+		lds r24, MilliCounter
+		lds r25, MilliCounter+1
+		adiw r25:r24, 1
+
+		cpi r24, low(781)
+		ldi temp, high(781)
+		cpc r25, temp
+		brne NotMilli
+		clr debounce
+		clear MilliCounter
+		rjmp EndIF
+
+	NotMilli:
+		sts MilliCounter, r24
+		sts MilliCounter+1, r25
 
 	EndIF:
         pop r24          
@@ -181,10 +173,15 @@ EXT_INT0:
 
 	isDebounced0:
 		inc debounce
-		inc isInputting
 		push temp
 		in temp, SREG 
 		push temp
+
+
+			//this section handles multiple button press
+			in temp, EIFR
+			cpi temp, 0b00000010
+			breq handleMultiplePress
 
 		ldi temp, 0b10000000	; tells the user they selected PB0
 		out PORTC, temp			; outputs it
@@ -209,10 +206,15 @@ EXT_INT1:
 
 	isDebounced1:
 		inc debounce
-		inc isInputting
 		push temp
 		in temp, SREG
 		push temp
+
+				//this section handles multiple button press
+				in temp, EIFR
+				cpi temp, 0b00000001
+				breq handleMultiplePress
+
 
 		lsl newOutput					; shifts everything by 1
 		ori newOutput, 0b00000001		; then or's with 1
@@ -231,6 +233,7 @@ EXT_INT1:
 		breq updateOutput
 		reti
 
+
 ; When we have fulfilled all 8 bits,
 ; we will update our currentoutput
 updateOutput:
@@ -238,12 +241,32 @@ updateOutput:
 	clr newOutput
 	clr nFlash
 	clr nBit
-	clr isInputting
 	reti
+
+handleMultiplePress:
+	ldi temp, 0b00000000
+	out EIMSK, temp
+	out PORTC, temp
+	ldi temp, 0xFF
+	cp xl, zl
+	cpc xh, zh
+	breq after
+
+		clearQueue:
+			st x+, temp
+			cp xl, zl
+			cpc xh, zh
+			brne clearQueue
+		
+		after:	
+			pop temp
+			out SREG, temp
+			pop temp
+			reti
 
 main:
 	clear SecondCounter
-	clear TempCounter
+	clear MilliCounter
 
 loop:
 	rjmp loop
